23999a548bddb933218cabf37dd8512134ab1d03
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2022-12-06 12:12:51 +0700

    day 06 - 2022

---
+++ 2022/06/part1.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+
+const path = resolve(import.meta.dir, 'input.txt')
+const res = await Bun.file(path).text()
+
+const len = 4
+let index = 0
+while (index < res.length - len) {
+  const signal = res.substring(index, index + len)
+  if (new Set(signal.split('')).size === len) {
+    break
+  }
+  index++
+}
+
+const result = index + len
+console.log(result)
